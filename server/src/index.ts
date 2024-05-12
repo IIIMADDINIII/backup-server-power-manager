1add30bc31ceb3819908ce95ed845abc7b21713e
IIIMADDINIII <martin@elend.name>
2024-05-13 00:44:08 +0200

    Adding /status api endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,44 +1,18 @@
-import fastifyStatic from "@fastify/static";
-import type { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
-import Fastify from 'fastify';
+import { PowerManager } from "./powerManager.js";
+import { createServerWithRoutes } from "./server.js";
 
 
+async function main(): Promise<number> {
+  const powerManager = new PowerManager();
+  const server = await createServerWithRoutes(powerManager);
+  await new Promise((res) => process.once("SIGINT", res));
+  await server.close();
+  await powerManager.close();
+  return 0;
+}
 
 
-
-
-
-
-const server = Fastify().withTypeProvider<TypeBoxTypeProvider>();
-
-server.register(fastifyStatic, { root: process.cwd(), serve: false });
-
-// index.html
-server.get("/", async function handler(_request, reply) {
-  reply
-    .type("text/html; charset=utf-8")
-    .send(`<!DOCTYPE html><html><head><script type="module" src="./index.js"></script></head></html>`);
-  return reply;
-});
-
-// index.js
-server.get("/index.js", async function handler(_request, reply) {
-  return reply.sendFile("./client/dist/index.js");
-});
-
-// index.js.map
-server.get("/index.js.map", async function handler(_request, reply) {
-  return reply.sendFile("./client/dist/index.js.map");
-});
-
-// Get server Power Status
-server.get("/status", async function handler(_request, response) {
-
-});
-
-try {
-  await server.listen({ port: 80, host: "0.0.0.0" });
-} catch (e) {
+main().then(process.exit).catch((e) => {
   console.error(e);
   process.exit(-1);
-}
+});
---
+++ server/src/server.ts
@@ -0,0 +1,36 @@
+import { PowerStatus } from "@app/common";
+import fastifyStatic from "@fastify/static";
+import type { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
+import Fastify from "fastify";
+import { PowerManager } from "./powerManager.js";
+
+export async function createServerWithRoutes(powerManager: PowerManager) {
+  const server = Fastify().withTypeProvider<TypeBoxTypeProvider>();
+  server.register(fastifyStatic, { root: process.cwd(), serve: false });
+
+  // index.html
+  server.get("/", async function handler(_request, reply) {
+    reply
+      .type("text/html; charset=utf-8")
+      .send(`<!DOCTYPE html><html><head><script type="module" src="./index.js"></script></head></html>`);
+    return reply;
+  });
+
+  // index.js
+  server.get("/index.js", async function handler(_request, reply) {
+    return reply.sendFile("./client/dist/index.js");
+  });
+
+  // index.js.map
+  server.get("/index.js.map", async function handler(_request, reply) {
+    return reply.sendFile("./client/dist/index.js.map");
+  });
+
+  // Get server Power Status
+  server.get("/status", { schema: { response: { 200: PowerStatus } } }, async function handler(_request, _response) {
+    return powerManager.getStatus();
+  });
+
+  await server.listen({ port: 80, host: "0.0.0.0" });
+  return server;
+}
